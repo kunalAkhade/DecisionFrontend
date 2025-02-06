bc1a0568c1a04f49d572117f1160f39faf346e62
kunal <bialkunal@gmail.com>
2025-02-06 10:50:52 +0530

    fix private component global auth

--- src/Auth/PrivateComponent.js
+++ src/Auth/PrivateComponent.js
@@ -1,12 +1,42 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AppContext } from "./AuthProvider";
 import LoginPage from "../pages/LoginPage";
 import { useParams } from "react-router-dom";
+import axios from "axios";
 
 export default function PrivateComponent({ children }) {
   const { token, setToken } = useContext(AppContext);
+  const [isValid, setValid] = useState(false);
+
+  useEffect(() => {
+    const checkValidToken = async () => {
+      try {
+        const response = await axios.get(
+          `${process.env.REACT_APP_API_URL}/api/User/profile`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+
+        if (response?.data?.username) {
+          setValid(true);
+        } else {
+          setValid(false);
+        }
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    checkValidToken();
+  }, [token]);
 
   console.log("obj", token);
 
-  return localStorage.getItem("token") ? <>{children}</> : <LoginPage />;
+  return localStorage.getItem("token") && isValid ? (
+    <>{children}</>
+  ) : (
+    <LoginPage />
+  );
 }
